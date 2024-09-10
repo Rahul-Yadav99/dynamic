36104c3733d4ba02446d0423eb005c11ba938d82
rahul <rahul352sj@gmail.com>
2024-09-10 23:06:56 +0530

    done

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 let productSection = document.getElementById("product-section");
-let dynamicCards = items.map(function(items){
-    return `
+let dynamicCards = items.map( (item, index) => {
+    return(`
     <div id="prodect-card" class="">
         <div class="">
-            <img class="h-[280px] w-[210px]" src="${items.img}" alt="">
+            <img class="h-[280px] w-[210px]" src="${item.img}" alt="">
         </div>
         <div class="">
-            <h1 class="text-lg font-bold">${items.name}</h1>
-            <p class="text-sm text-gray-500">${items.dis}</p>
-            <h1 class="text-base font-bold">${items.price} 
+            <h1 class="text-lg font-bold">${item.name}</h1>
+            <p class="text-sm text-gray-500">${item.dis}</p>
+            <h1 class="text-base font-bold">${item.price} 
             <span class="line-through text-sm text-gray-500">
-            ${items.actualPrice}
+            ${item.actualPrice}
             </span>
             <span class="text-base font-semibold text-red-500">
-            (${items.discount}%OFF)
+            (${item.discount}%OFF)
             </span>
             </h1>
         </div> 
     </div>`
-})
+)}).join("")
+
+ 
 productSection.innerHTML = dynamicCards;
